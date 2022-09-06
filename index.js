dc7163af9656124d65fc3ac1024354d6011f21f2
RugvedR <rugvedr9@gmail.com>
2022-09-06 18:19:57 +0530

    posts route set up, posts update and delete

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const dotenv = require("dotenv")
 const morgan = require("morgan")
 const userRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
+const postRoute = require("./routes/posts")
+
 
 
 dotenv.config();
@@ -23,6 +25,9 @@ app.use(morgan("common"));
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+
+
 
 
 app.listen(8800, ()=>{
